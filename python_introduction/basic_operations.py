9632525af021df257e782f640db4fdf73b0d31a6
Mohgar <mohamedhgar007@gmail.com>
2024-06-30 19:43:47 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum1 = number1 + number2
+sum2= number1 - number2
+multiplication = number1 * number2
+print("Addition of 10 and 5 is", sum1)
+print("Subtraction of 10 and 5 is", sum2)
+print("Multiplication of 10 and 5 is" , multiplication)
